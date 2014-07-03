93e004a24aee78ce8d8623a7766597808090be25
nelson jr <ircnelson@gmail.com>
2014-07-02 23:55:14 -0400

    initial index

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,46 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        jsDest: 'public/js/libs',
+
         copy: {
+
             requirejs: {
                 src: 'bower_components/requirejs/require.js',
-                dest: 'public/js/libs/requirejs/require.js'
+                dest: '<%= jsDest %>/requirejs/require.js'
+            },
+
+            bootstrap: {
+
+                files: [
+                    {
+                        src: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                        dest: '<%= jsDest %>/twbs/bootstrap.min.js'
+                    },
+                    {
+                        src: 'bower_components/bootstrap/dist/css/*.min.css',
+                        dest: 'public/css/',
+                        expand: true,
+                        flatten: true
+                    },
+                    {
+                        src: 'bower_components/bootstrap/dist/fonts/*',
+                        dest: 'public/fonts/',
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    }
+                ]
+            },
+
+            jquery: {
+                src: 'bower_components/jquery/dist/jquery.min.js',
+                dest: '<%= jsDest %>/jquery/jquery.min.js'
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    // Load plugins.
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
